601b7f5d4cb6710871a5b8492f633e54f37c1ff1
andinet2019 <andinetmn@gmail.com>
2022-02-27 16:47:23 -0600

    fourth commit

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from sendemail import send_mail
+from dotenv import load_dotenv
+load_dotenv('.env') #the path to your .env file (or any other file of environment variables y
 app = Flask(__name__)
-ENV = 'prod'
+ENV = 'dev'
 if ENV == 'dev':
     app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ('DB_DEV_URL')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DB_DEV_URL')
 else:
     app.debug = True
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DB_PROD_URL']
@@ -64,7 +66,6 @@ def submit():
             send_mail(customer, realtor, address, rating, comments)
             return render_template("success.html")
         return render_template("index.html",message="You have already submitted feedback")
-
 
 if __name__ == "__main__":
     app.run()
