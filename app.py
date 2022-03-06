20e8295e4d8771c62e6d346c5c3f0a3ce72b7714
andinet2019 <andinetmn@gmail.com>
2022-03-06 03:45:40 -0600

    commnet

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import os
 from sendemail import send_mail
 from dotenv import load_dotenv
-load_dotenv('.env') #the path to your .env file (or any other file of environment variables y
+import os
+
+# for using secrets in .env
+load_dotenv()
+
 app = Flask(__name__)
-ENV = 'dev'
+
+ENV = 'prod'
+
 if ENV == 'dev':
     app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DB_DEV_URL')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DEV_DB_URL')
+
+
 else:
     app.debug = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DB_PROD_URL']
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_PROD_URL")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
 class Survey(db.Model): 
-    __tablename__ = "survey"
+    __tablename__ = "survey_model"
     id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    realtor = db.Column(db.String(200))
-    address = db.Column(db.String)
+    student = db.Column(db.String(200), unique=True)
+    instructor = db.Column(db.String(200))
+    cohort_name = db.Column(db.String)
     rating = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-    def __init__(self, customer, realtor, address, rating, comments):
-        self.customer = customer
-        self.realtor = realtor
-        self.address = address
+    def __init__(self, student, instructor, cohort_name, rating, comments):
+        self.student = student
+        self.instructor = instructor
+        self.cohort_name = cohort_name
         self.rating = rating
         self.comments = comments
 
@@ -51,21 +58,23 @@ def submit():
     :return:
     """
     if request.method == "POST":
-        customer = request.form["customer"]
-        realtor = request.form["realtor"]
-        address = request.form["address"]
+        student = request.form["student"]
+        instructor = request.form["instructor"]
+        cohort_name = request.form["cohort_name"]
         rating = request.form["rating"]
         comments = request.form["comments"]
+
         # validating if user has a valid input
-        if customer == "" or realtor == "" or comments == "":
-            return render_template('index.html', message="Please enter the required fields")
-        if db.session.query(Survey).filter(Survey.customer == customer).count() == 0:
-            data = Survey(customer, realtor, address, rating, comments)
+        if student == "" or instructor == "" or comments == "":
+            return render_template('index.html', message="Please enter the required fields!")
+        if db.session.query(Survey).filter(Survey.student == student).count() == 0:
+            data = Survey(student, instructor, cohort_name, rating, comments)
             db.session.add(data)
             db.session.commit()
-            send_mail(customer, realtor, address, rating, comments)
+            send_mail(student, instructor, cohort_name, rating, comments)
             return render_template("success.html")
-        return render_template("index.html",message="You have already submitted feedback")
+        return render_template("index.html", message="You have already submitted the feedback.Have a good day!")
+
 
 if __name__ == "__main__":
     app.run()
--- sendemail.py
+++ sendemail.py
@@ -1,20 +1,23 @@
 import smtplib
 from email.mime.text import MIMEText
-import os
 from dotenv import load_dotenv
+import os
 
-def send_mail(customer, realtor, address, rating, comments):
-    port = 25
-    smtp_server = os.environ.get('smtp_server')
-    login = os.get('login')
-    password = os.get('password')
-    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Realtor: {realtor}" \
-              f"</li><li>Address: {address}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+load_dotenv()
+
+
+def send_mail(student, instructor, cohort_name, rating, comments):
+    port = 2525
+    smtp_server = "smtp.mailtrap.io"
+    login = os.getenv('login')
+    password = os.getenv('password')
+    message = f"<h3>New Feedback Submission Received</h3><ul><li>Student Name: {student}</li><li>Instructor Name: {instructor}" \
+              f"</li><li>Cohort Name: {cohort_name}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
     sender_email = 'email1@example.com'
     receiver_email = 'email2@example.com'
     msg = MIMEText(message, 'html')
-    msg['Subject'] = 'MNHome RealEstate Customer Feedback'
+    msg['Subject'] = 'TwinsCodingCamp Students Feedback'
     msg['From'] = sender_email
     msg['To'] = receiver_email
 
